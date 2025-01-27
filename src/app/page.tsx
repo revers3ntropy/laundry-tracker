62c367441e2fc7122f03da088bf15876b4c100d4
revers3ntropy <josephcoppin@gmail.com>
2025-01-27 10:35:15 +0000

    make request to API and show result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import { Suspense, use } from "react";
+
+function Comments({ dataPromise }: { dataPromise: Promise<unknown> } ) {
+  const data = use(dataPromise);
+  return <div>{JSON.stringify(data)}</div>;
+}
+
+async function loadData() {
+  const res = await fetch('https://api.alliancelabs.com/washAlerts/machines?organisationID=652210');
+  return res.json();
+}
+
 export default function () {
   return <main>
-    home
+    <Suspense fallback={<div>Loading...</div>}>
+      <Comments dataPromise={loadData()} />
+    </Suspense>
   </main>
 }
