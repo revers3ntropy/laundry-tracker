f7e8b1cb1bb983fc553d14db3b0bee51da933c3b
revers3ntropy <josephcoppin@gmail.com>
2025-01-29 10:33:29 +0000

    title to landing page and info about number of available machines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Suspense, use } from 'react';
 import { fmtTimeDurationMinutes } from '@/app/util';
-import type { Machine, MachineStatus} from '@/app/machineData';
+import type { Machine, MachineStatus } from '@/app/machineData';
 import { fmtMachineType, loadMachinesData, MachineState, MachineType } from '@/app/machineData';
 import { ColouredDot } from '@/app/ColouredDot';
 
@@ -23,7 +23,6 @@ function MachineStateInfo({ state }: { state: MachineStatus }) {
     return (
         <>
             <p className="flex gap-2 justify-start items-center">
-                {' '}
                 <ColouredDot colour="orange" /> In use
             </p>
             <p>{fmtTimeDurationMinutes(state.remainingSeconds)} remaining</p>
@@ -33,7 +32,7 @@ function MachineStateInfo({ state }: { state: MachineStatus }) {
 
 function MachineInfo({ machine }: { machine: Machine }) {
     return (
-        <div className="p-2 border-solid border rounded-xl border-gray-600 w-40 h-32">
+        <div className="p-2 border-solid border-2 rounded-xl border-gray-600 w-40 h-28">
             {fmtMachineType(machine.type)}
             <MachineStateInfo state={machine.status} />
         </div>
@@ -48,8 +47,23 @@ function PageWithData({ dataPromise }: { dataPromise: Promise<Machine[]> }) {
         }
         return a.type === MachineType.WASHER ? -1 : 1;
     });
+    const numAvailableWashers = sortedData.filter(
+        m => m.type === MachineType.WASHER && m.status.type === MachineState.AVAILABLE
+    ).length;
+    const numAvailableDryers = sortedData.filter(
+        m => m.type === MachineType.DRYER && m.status.type === MachineState.AVAILABLE
+    ).length;
     return (
         <div>
+            <div className="pb-8 pt-2">
+                <h1 className="text-3xl">Rootes Laundromat</h1>
+                <p>
+                    <span className="text-xl">{numAvailableWashers}</span> washers available
+                </p>
+                <p>
+                    <span className="text-xl">{numAvailableDryers}</span> dryers available
+                </p>
+            </div>
             <div className="flex flex-wrap gap-2">
                 {sortedData.map(machine => (
                     <MachineInfo key={machine.id} machine={machine} />
