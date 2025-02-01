8d40aa428a114ea2cdf656dcdbaef508ef41f664
revers3ntropy <josephcoppin@gmail.com>
2025-02-01 13:50:19 +0000

    link to Jonathan Iles portfolio in footer

--- src/lib/Footer.tsx
+++ src/lib/Footer.tsx
@@ -6,15 +6,18 @@ export function Footer() {
         <footer className="flex items-center gap-4 flex-col py-4 text-gray-400">
             <p>
                 Created by{' '}
-                <a href="https://josephcoppin.com" target="_blank" className="underline">
+                <a href="https://josephcoppin.com" target="_blank" className="underline hover:text-white">
                     Joseph Coppin
                 </a>{' '}
-                and Jonathan Iles
+                and{' '}
+                <a href="https://orcid.org/0009-0006-1364-9214" target="_blank" className="underline hover:text-white">
+                    Jonathan Iles
+                </a>{' '}
             </p>
             <p>
                 <a
                     href="https://github.com/revers3ntropy/laundry-tracker"
-                    className="flex items-center justify-center gap-2"
+                    className="flex items-center justify-center gap-2 hover:text-white"
                     target="_blank"
                 >
                     <Icon path={mdiGithub} size={1.1} className="inline" /> Source Code
