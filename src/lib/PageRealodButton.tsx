f46a1eb1752b4ca8c451272338315d00756c3637
revers3ntropy <josephcoppin@gmail.com>
2025-02-01 01:00:54 +0000

    shrink size of reload button

--- src/lib/PageRealodButton.tsx
+++ src/lib/PageRealodButton.tsx
@@ -6,7 +6,7 @@ import { mdiReload } from '@mdi/js';
 export function PageReloadButton() {
     return (
         <button onClick={() => location.reload()}>
-            <Icon path={mdiReload} size={3} />
+            <Icon path={mdiReload} size={1.8} />
         </button>
     );
 }
